8e4b42284a3a60cce9c61a2de35ae0a89fda7fae
Paul Llanque <llanquepol@gmail.com>
2023-05-18 16:46:49 -0500

    add: Producto model

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+import mongoose, { Schema, model, models } from 'mongoose';
+
+const ProductSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+});
+
+export const Product = mongoose.models.Product ||  model('Product', ProductSchema);
