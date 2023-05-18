bb209402e07904edde4bd0d34b015232625aac40
Paul Llanque <llanquepol@gmail.com>
2023-05-17 23:11:44 -0500

    add new page

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,10 @@
-import Layout from "@/components/Layout";
+import Layout from '@/components/Layout';
+import Link from 'next/link';
 
 export default function Products() {
-  return <Layout>Products</Layout>;
+  return (
+    <Layout>
+      <Link href='/products/new' className='bg-gray-300 py-1 px-2 rounded-md'>Add new products</Link>
+    </Layout>
+  );
 }
