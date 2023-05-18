64e935d7b0ca06770901df464ffb6124358a9d71
Paul Llanque <llanquepol@gmail.com>
2023-05-17 22:04:03 -0500

    update: dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
 import Layout from '@/components/Layout';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Home() {
-  return <Layout>test</Layout>;
+  const { data: session } = useSession();
+
+  return (
+    <Layout>
+      <div className='text-cyan-700 flex justify-between'>
+        <h2>
+          Hello,
+          <b> {session?.user?.name}</b>
+        </h2>
+        <div className='flex bg-gray-500 gap-1 text-white rounded-lg overflow-hidden'>
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            width={24}
+            height={24}
+            className='w-8 h-8'
+          />
+          <span className='py-1 px-2'>{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
