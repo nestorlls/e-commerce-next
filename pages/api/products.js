ba431d9ee4ca81817406303e24e8fd7051385e99
Paul Llanque <llanquepol@gmail.com>
2023-05-18 16:47:25 -0500

    add: product POST

---
+++ pages/api/products.js
@@ -0,0 +1,18 @@
+import mongooseConnection from '@/lib/mongoose';
+import { Product } from '@/models/Product';
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  await mongooseConnection();
+  if (method === 'POST') {
+
+    const { title, description, price } = req.body;
+    const product = await Product.create({
+      title,
+      description,
+      price,
+    });
+    res.status(201).json({message: 'Proucts was create succefully', product});
+  }
+}
