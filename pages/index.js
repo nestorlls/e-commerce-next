767cbdef6e1e4624c62f2e9d38c83064d46b3d74
Paul Llanque <llanquepol@gmail.com>
2023-05-17 21:36:20 -0500

    add: pages

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,5 @@
-import { useSession, signIn, signOut } from 'next-auth/react';
-import Image from 'next/image';
-import googleicon from '../public/icons8-google.svg';
+import Layout from '@/components/Layout';
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className='bg-cyan-600 w-screen h-screen flex items-center'>
-        <div className='text-center w-full flex justify-center'>
-          <button
-            className='bg-white p-2 rounded-lg px-4 flex gap-2'
-            onClick={() => signIn('google')}
-          >
-            <Image src={googleicon} alt='google icon' width={24} height={24} />
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <div>Logged with name {session?.user.email} </div>
-    </>
-  );
+  return <Layout>test</Layout>;
 }
