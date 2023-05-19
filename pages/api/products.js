2c404e00bd6f700313a4d8192eef5ecfffb48292
Paul Llanque <llanquepol@gmail.com>
2023-05-18 21:58:02 -0500

    update: product api

--- pages/api/products.js
+++ pages/api/products.js
@@ -31,4 +31,11 @@ export default async function handler(req, res) {
     await Product.updateOne({ _id }, { title, description, price });
     res.json(true);
   }
+
+  if (method === 'DELETE') {
+    if (req.query?.id) {
+      await Product.deleteOne({ _id: req.query?.id });
+      res.json(true);
+    }
+  }
 }
