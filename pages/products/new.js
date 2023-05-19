6d99179da369462da0b4e383be4d344c286abdee
Paul Llanque <llanquepol@gmail.com>
2023-05-18 20:57:24 -0500

    refactor: new product page

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,72 +1,12 @@
 import Layout from '@/components/Layout';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
+import ProductForm from '@/components/ProductForm';
 
-export default function New() {
-  const initialValue = {
-    title: '',
-    description: '',
-    price: '',
-  };
 
-  const router = useRouter();
-  const [dataProduct, setDataProduct] = useState(initialValue);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDataProduct({
-      ...dataProduct,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post(`/api/products`, dataProduct);
-    router.push('/products');
-  };
-
+export default function NewProduct() {
   return (
     <Layout>
       <h1>New Product</h1>
-      <form onSubmit={handleSubmit}>
-        <div className='input-container'>
-          <label htmlFor='title'>Product name</label>
-          <input
-            type='text'
-            name='title'
-            id='title'
-            placeholder='Product name'
-            value={dataProduct.title}
-            onChange={handleChange}
-          />
-        </div>
-        <div className='input-container'>
-          <label htmlFor='description'>Description</label>
-          <textarea
-            id='description'
-            name='description'
-            placeholder='description'
-            value={dataProduct.description}
-            onChange={handleChange}
-          />
-        </div>
-        <div className='input-container'>
-          <label htmlFor='price'>Price (in USD)</label>
-          <input
-            type='number'
-            id='price'
-            name='price'
-            placeholder='price'
-            value={dataProduct.price}
-            onChange={handleChange}
-          />
-        </div>
-        <button className='btn-primary' type='submit' onSubmit={handleSubmit}>
-          Save
-        </button>
-      </form>
+      <ProductForm />
     </Layout>
   );
 }
