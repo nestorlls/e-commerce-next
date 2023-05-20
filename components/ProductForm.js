e9c24094902825abcd7577fea4386abd1c276833
Paul Llanque <llanquepol@gmail.com>
2023-05-20 12:24:11 -0500

    add: upload photos

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -2,11 +2,28 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
+const uploadIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='h-6 w-6'>
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5'
+    />
+  </svg>
+);
+
 export default function ProductForm({
   _id,
   title: exitingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const initialValue = {
     title: exitingTitle ?? '',
@@ -50,6 +67,17 @@ export default function ProductForm({
             value={dataProduct.title}
             onChange={handleChange}
           />
+        </div>
+        <div>
+          <label>Photo</label>
+          <div className='mb-4'>
+            <label className='flex h-24 w-24 flex-col items-center justify-center gap-2 rounded-lg border bg-gray-200 text-sm text-gray-500 hover:bg-gray-300'>
+              {uploadIcon}
+              <span>Upload</span>
+              <input type='file' className='hidden' />
+            </label>
+            {!images?.length && <div>No photos in this product</div>}
+          </div>
         </div>
         <div className='input-container'>
           <label htmlFor='description'>Description</label>
